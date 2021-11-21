271c14f4b196da161836acc9ae9176ddfd7b4263
Josue Rodriguez <maybejosue@gmail.com>
2021-11-21 15:55:39 -0800

    Adds default theme to main app

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,28 @@
+import { createTheme, ThemeProvider, Paper } from "@mui/material";
+import { Home } from "./components";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#061834",
+    },
+    secondary: {
+      main: "#0046FF",
+    },
+    background: {
+      default: "#F4F5FC",
+    },
+  },
+});
+
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Paper>
+        <Home />
+      </Paper>
+    </ThemeProvider>
+  );
 }
 
 export default App;
