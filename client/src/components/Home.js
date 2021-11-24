3ba4d621ac708c1d9c4036c152c1cbb920164adb
Josue Rodriguez <maybejosue@gmail.com>
2021-11-24 15:44:09 -0800

    Adds AppBackground Styles

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,24 @@
 import React from "react";
 import Navbar from "./navbar/Navbar.js";
+import { Box } from "@mui/material";
 
-export default function Home() {
+import { makeStyles, createStyles } from "@mui/styles";
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    appBackground: {
+      backgroundColor: theme.palette.background.default,
+      height: "100vh",
+    },
+  })
+);
+
+export default function Home({ theme }) {
+  const classes = useStyles(theme);
   return (
-    <React.Fragment>
+    <Box className={classes.appBackground}>
       <Navbar />
       Home content
-    </React.Fragment>
+    </Box>
   );
 }
