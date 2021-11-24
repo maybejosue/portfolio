0599b03054a8bbc009efc327ead654633751053f
Josue Rodriguez <maybejosue@gmail.com>
2021-11-24 08:56:00 -0800

    Adds Logo component

---
+++ client/src/components/navbar/Logo.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Typography, Box } from "@mui/material";
+import { makeStyles, createStyles } from "@mui/styles";
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    logo: {
+      flexGrow: 1,
+      display: "flex",
+      alignItems: "center",
+    },
+    firstName: {
+      fontWeight: "600",
+    },
+  })
+);
+
+export default function Logo() {
+  const classes = useStyles();
+  return (
+    <Typography variant="subtitle1" className={classes.logo}>
+      <Box component="span" className={classes.firstName}>
+        Josue
+      </Box>
+      Rodriguez
+    </Typography>
+  );
+}
