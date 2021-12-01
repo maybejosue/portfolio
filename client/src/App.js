ccdecbcc8dcb7cdd6a6ccbdd99bcb702963ecff6
Josue Rodriguez <maybejosue@gmail.com>
2021-12-01 11:39:39 -0800

    adds trigger to switch between dark and light mode
    adds local storage key and value to save user preferences

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,42 @@
+import { useState } from "react";
 import { createTheme, ThemeProvider } from "@mui/material";
 import { Home } from "./components";
 
-const lightTheme = createTheme({
+const getDesignTokens = (mode) => ({
   breakpoints: {
     values: {
       md: 800,
     },
   },
   palette: {
-    primary: {
-      main: "#061834",
-    },
-    secondary: {
-      main: "#0046FF",
-    },
-    background: {
-      default: "#F4F5FC",
-    },
+    mode,
+    ...(mode === "light"
+      ? {
+          // palette values for light mode
+          primary: {
+            main: "#061834",
+          },
+          secondary: {
+            main: "#0046FF",
+          },
+          background: {
+            default: "#F4F5FC",
+          },
+        }
+      : {
+          // palette values for dark mode
+          mode: "dark",
+          primary: {
+            main: "#E6E5E8",
+          },
+          secondary: {
+            main: "#0046FF",
+          },
+          background: {
+            paper: "#1E293D",
+            default: "#141221",
+          },
+        }),
   },
   components: {
     MuiTimelineItem: {
@@ -31,43 +51,16 @@ const lightTheme = createTheme({
   },
 });
 
-const darkTheme = createTheme({
-  breakpoints: {
-    values: {
-      md: 800,
-    },
-  },
-  palette: {
-    mode: "dark",
-    primary: {
-      main: "#E6E5E8",
-    },
-    secondary: {
-      main: "#0046FF",
-    },
-    background: {
-      paper: "#1E293D",
-      default: "#141221",
-    },
-  },
-  components: {
-    MuiTimelineItem: {
-      styleOverrides: {
-        root: {
-          ":before": {
-            content: "none",
-          },
-        },
-      },
-    },
-  },
-});
-
-let theme = lightTheme;
 function App() {
+  const [mode, setMode] = useState(
+    localStorage.getItem("mode") === "dark" ? "dark" : "light"
+  );
+
+  const theme = createTheme(getDesignTokens(mode), [mode]);
+
   return (
     <ThemeProvider theme={theme}>
-      <Home />
+      <Home mode={mode} setMode={setMode} />
     </ThemeProvider>
   );
 }
