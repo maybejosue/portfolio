ca81d3e6d6944d18f79c8081a6c0c87fac5bb80b
Josue Rodriguez <maybejosue@gmail.com>
2022-03-24 15:25:07 -0700

    converts message components to typescript

--- client/src/components/experience/Experience.js
+++ client/src/components/experience/Experience.js
@@ -1,58 +1,64 @@
-import React, { useState } from "react";
-import Section from "../helpers/Section.js";
-import { Typography, Tabs, Tab, Box } from "@mui/material";
-import { makeStyles, createStyles } from "@mui/styles";
-import TabPanel from "./TabPanel.js";
-import useMediaQuery from "@mui/material/useMediaQuery";
-import UpdateMessage from "../helpers/UpdateMessage.js";
-import { jobSearchStatus } from "../../content/";
-import { workHistory } from "../../content/";
+import React, { useState } from 'react'
+import Section from '../helpers/Section.js'
+import { Typography, Tabs, Tab, Box } from '@mui/material'
+import { makeStyles, createStyles } from '@mui/styles'
+import TabPanel from './TabPanel.js'
+import useMediaQuery from '@mui/material/useMediaQuery'
+import UpdateMessage from '../helpers/UpdateMessage.tsx'
+import { jobSearchStatus } from '../../content/'
+import { workHistory } from '../../content/'
 
 const useStyles = makeStyles((theme) =>
-  createStyles({
-    tabsContainer: {
-      [theme.breakpoints.up("md")]: {
-        display: "flex",
-      },
-    },
-    tabs: {
-      marginBottom: "2rem",
-    },
-    tab: {
-      textTransform: "capitalize !important",
-    },
-  })
-);
+    createStyles({
+        tabsContainer: {
+            [theme.breakpoints.up('md')]: {
+                display: 'flex',
+            },
+        },
+        tabs: {
+            marginBottom: '2rem',
+        },
+        tab: {
+            textTransform: 'capitalize !important',
+        },
+    })
+)
 
 export default function Experience({ theme }) {
-  const classes = useStyles(theme);
-  const [value, setValue] = useState(0);
-  const matches = useMediaQuery((theme) => theme.breakpoints.down("md"));
+    const classes = useStyles(theme)
+    const [value, setValue] = useState(0)
+    const matches = useMediaQuery((theme) => theme.breakpoints.down('md'))
 
-  return (
-    <Section sectionName="experience">
-      <Typography paragraph>Work History</Typography>
-      {jobSearchStatus && <UpdateMessage message="Currently Job Searching!" />}
+    return (
+        <Section sectionName="experience">
+            <Typography paragraph>Work History</Typography>
+            {jobSearchStatus && (
+                <UpdateMessage message="Currently Job Searching!" />
+            )}
 
-      <Box className={classes.tabsContainer}>
-        <Tabs
-          className={classes.tabs}
-          value={value}
-          onChange={(e, v) => setValue(v)}
-          variant={matches ? "scrollable" : "standard"}
-          indicatorColor="secondary"
-          scrollButtons
-          allowScrollButtonsMobile
-          aria-label="work history tabs"
-          orientation={matches ? "horizontal" : "vertical"}
-        >
-          {workHistory.map(({ companyName }, i) => (
-            <Tab label={companyName} key={i} className={classes.tab} />
-          ))}
-        </Tabs>
+            <Box className={classes.tabsContainer}>
+                <Tabs
+                    className={classes.tabs}
+                    value={value}
+                    onChange={(e, v) => setValue(v)}
+                    variant={matches ? 'scrollable' : 'standard'}
+                    indicatorColor="secondary"
+                    scrollButtons
+                    allowScrollButtonsMobile
+                    aria-label="work history tabs"
+                    orientation={matches ? 'horizontal' : 'vertical'}
+                >
+                    {workHistory.map(({ companyName }, i) => (
+                        <Tab
+                            label={companyName}
+                            key={i}
+                            className={classes.tab}
+                        />
+                    ))}
+                </Tabs>
 
-        <TabPanel workHistory={workHistory[value]} />
-      </Box>
-    </Section>
-  );
+                <TabPanel workHistory={workHistory[value]} />
+            </Box>
+        </Section>
+    )
 }
