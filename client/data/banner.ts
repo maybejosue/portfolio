0ed0d10b9db770b33f54819887ff96d4aac7374c
Josue Rodriguez <maybejosue@gmail.com>
2023-05-21 12:00:08 -0700

    update

--- client/data/banner.ts
+++ client/data/banner.ts
@@ -1,7 +1,7 @@
 export const banner = {
     headline: 'Hey, my name is',
     name: 'Josue Rodriguez',
-    oneliner: 'I craft and build software solutions',
+    oneliner: 'I build software solutions',
     status: 'Feel free to explore my portfolio and discover more about my various social media profiles through the button below.',
     link: 'https://linktr.ee/maybejosue',
 }
